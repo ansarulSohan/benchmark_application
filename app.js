d9fe3f25587403b3ec61094e991571bacc09fd8a
ansarulSohan <ansarulsohan@gmail.com>
2025-03-18 16:29:39 +0100

    fixed default number problem

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const { performance } = require("perf_hooks");
 const express = require("express");
 const app = express();
 
+const DEFAULT_NUMBER = 99999999;
+
 const bubbleSortBenchmark = require("./benchmarks/bubbleSort");
 const semiPrimeBenchmark = require("./benchmarks/semiPrime");
 const fibonacciBenchmark = require("./benchmarks/fibonacci");
@@ -11,21 +13,31 @@ app.get("/", (req, res) => {
 });
 
 app.get("/bubbleSort", (req, res) => {
-  const number = req.query.number | Number.MAX_SAFE_INTEGER;
+  const number = req.query.number;
+  if (!number) {
+    number = DEFAULT_NUMBER;
+  }
   bubbleSortBenchmark(number);
   res.status(200).send("Bubble sort benchmark completed");
 });
 
 app.get("/semiPrime", (req, res) => {
-  const number = parseInt(req.query.number) | Number.MAX_SAFE_INTEGER;
-  console.log(number);
-  semiPrimeBenchmark(number);
+  console.log(req.query.number);
+  const num = parseInt(req.query.number);
+  if (!num) {
+    num = DEFAULT_NUMBER;
+  }
+  console.log(num);
+  semiPrimeBenchmark(num);
   res.status(200).send("Semi prime benchmark completed");
 });
 
 app.get("/fibonacci", (req, res) => {
-  const number = req.query.number | Number.MAX_SAFE_INTEGER;
+  const number = req.query.number;
   console.log(number);
+  if (!number) {
+    number = DEFAULT_NUMBER;
+  }
   fibonacciBenchmark(number);
   res.status(200).send("Fibonacci benchmark completed");
 });
